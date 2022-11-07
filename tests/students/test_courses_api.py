7e38d74ebf91587b3f6878087a56a3143d8f4c23
Tryd0g0lik <work80@mail.ru>
2022-11-07 13:56:16 +0700

    Created the basis test, made only the  GET request

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -1,2 +1,17 @@
+from django.contrib.auth import get_user_model
+import pytest
+from rest_framework.test import APIClient
+
+@pytest.mark.django_db
 def test_example():
-    assert False, "Just test example"
+    #Arrange
+    clients = APIClient()
+
+    #Act
+    response = clients.get('/courses/')
+
+    # Accert
+    assert response.status_code == 200
+    data = response.json()
+    assert  len(data) == 0
+    # assert True
