22e015b38b1ad3b96e2c5dae8935567862682857
Tryd0g0lik <work80@mail.ru>
2022-11-09 16:20:53 +0700

    may be completed

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -17,8 +17,9 @@ def stude():
     return baker.make(Course)
 
 @pytest.fixture
-def getUser():
-    return User.objects.create_user('admin')
+def user():
+    user = User.objects.create_user('admin')
+    return user.id
 
 @pytest.mark.django_db(
   databases='netology_django_testing',
@@ -86,27 +87,65 @@ def test_example(
 def test_example_post(
   name_stude = get_name_random,
   title = get_courses_random,
-  api_client = api_client
+  api_client = api_client,
+  user_id = user
   ):
-
-  # Arrange
-  baker.make(
-    "students.Student",
-    name = name_stude,
- )
-
-  baker.make(
-    "students.Course",
-     name = title,
-    )
-
-  # Act
   api_client = APIClient()
 
-  response = api_client.get('/courses/')
+  response = api_client.post('/courses/', data={
+    'user' : user_id,
+    'name' : title,
+    'student' : name_stude,
+  })
 
   # Accert
-  assert response.status_code == 200
-  data = response.json()
-  assert len(data[0]) != 0
-  assert data
+  assert response.status_code == 201
+  assert Course.objects.count != 0
+
+# @pytest.mark.django_db()
+# def test_example_put(
+#   name_stude=get_name_random,
+#   title=get_courses_random,
+#   api_client=api_client,
+#   user_id=user
+# ):
+#   # Arrange
+#   name_studes = baker.make(
+#     "students.Student",
+#     name=name_stude,
+#   )
+#
+#   titles = baker.make(
+#     "students.Course",
+#     name=title,
+#   )
+#
+#   api_client = APIClient()
+#
+#   student = api_client.put(
+#     '/student/', data={
+#       'user': user_id,
+#       'name': name_studes,
+#     }
+#   )
+#   cours = api_client.post(
+#     '/courses/', data={
+#     'user': user_id,
+#     'name': titles,
+#     'student': name_studes.id,
+#     }
+#   )
+#
+#   old_data = student.get(pk=0)
+#   response = api_client.put(
+#     '/student/', data={
+#       old_data : 'PUT_request',
+#     }
+#   )
+#
+#
+#
+#   # Accert
+#   assert response.status_code == 204
+#   assert print(response)
+#   # assert Course.objects.count != 0
