69c540d9f11614ddc8e62bd65e949b2938a4e57b
Tryd0g0lik <work80@mail.ru>
2022-11-14 09:50:35 +0700

    the file of students/urls.py path added

---
+++ students/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from students.views import CoursesViewSet, StudentViewSet
+
+router = DefaultRouter()
+router.register("courses", CoursesViewSet, basename="courses")
+router.register('student', StudentViewSet, basename="student")
+
+
+urlpatterns = [
+
+] + router.urls
